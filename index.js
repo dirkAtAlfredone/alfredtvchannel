3490fd773a272b35f07af7055985e7582f0e0c0a
dirkelson <dirk@alfredone.ca>
2025-08-18 14:01:40 -0400

    add verification process

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const path = require("path");
 const mongoose = require("mongoose");
 const axios = require("axios");
 const { Agent } = require("https");
+const jwt = require("jsonwebtoken");
 
 const DB = process.env.DB;
 const UISP_KEY = process.env.UISP_KEY;
 const API_URL = process.env.UISP_URL;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const app = express();
 
@@ -41,12 +43,18 @@ app.get("/user/:id", async (req, res) => {
         const {data: client} = await axios.get(`${API_URL}/api/v1.0/clients/${req.params.id}`, {headers, httpsAgent});
         const channelSubscriptions = subscriptions.filter(subscription => subscription.name === 'ALFRED TV | Canada Channel Line Up');
         if(channelSubscriptions.length !== 0){
+
             const user = {
                 firstName: client.firstName,
                 lastName: client.lastName,
-                organizationName: client.organizationName
+                organizationName: client.organizationName,
+                subscriptionIds: channelSubscriptions.map(subs => subs.id),
+                clientId: client.id
             };
-            res.status(200).json(user);
+
+            const token = jwt.sign(user, JWT_SECRET, {expiresIn: "10s"});
+
+            res.status(200).json({user, token});
         }
         else {
             res.status(400).json({
@@ -59,7 +67,47 @@ app.get("/user/:id", async (req, res) => {
             message: "Internal Server Error"
         });
     }
-})
+});
+
+app.get("/token/validate", (req, res) => {
+    try{
+        const authorization = req.headers.authorization;
+        const token = authorization?.split(" ")[1];
+        const decoded = jwt.verify(token, JWT_SECRET);
+        const currentTime = Math.floor(Date.now() / 1000);
+        const timeLeft = decoded.exp - currentTime;
+        if(timeLeft >= 0){
+            res.status(202).json({
+                timeLeft,
+                token
+            });
+        }
+        else{
+            throw new Error("Unhandled Error");
+        }
+    }
+    catch(e){
+        // check device details if matches in db
+        if(e.name === "TokenExpiredError" && e.message === "jwt expired"){
+            const authorization = req.headers.authorization;
+            const token = authorization?.split(" ")[1];
+            const decoded = jwt.decode(token, JWT_SECRET);
+            const { firstName, lastName, organizationName, subscriptionIds } = decoded;
+
+            const matches = true;
+            if(matches){
+                const newAuth = jwt.sign({ firstName, lastName, organizationName, subscriptionIds }, JWT_SECRET, {expiresIn: "10s"});
+                res.status(202).json({
+                    token: newAuth,
+                    timeLeft: 11
+                });
+            }
+        }
+        else{
+            res.status(500).json(e);
+        }
+    }
+});
 
 app.listen(3002, () => {
     console.log("Started in port 3002...");
